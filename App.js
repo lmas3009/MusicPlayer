17e1777f7991b971ed3ec9b292fc9466406d456f
lmas <30lmas09@gmail.com>
2020-08-31 14:07:50 +0530

    'init'

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image, TouchableOpacity } from 'react-native';
+import Logo1 from './assets/logo1.png'
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from'@react-navigation/stack'
+import MainPage from './Views/Login Screens/mainpage';
 
-export default function App() {
+ class splash extends React.Component {
+   constructor(props){
+     super(props);
+   }
+   render(){
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={()=>this.props.navigation.navigate('MainPage')}>
+          <Image style={styles.logo} source={Logo1}/>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
+    );
+   }
+}
+
+
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Splash" component={splash} options={{headerShown: false}}/>
+        <Stack.Screen name="MainPage" component={MainPage} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo:{
+    height: 150,
+    width: 150
+  }
 });
+
+
